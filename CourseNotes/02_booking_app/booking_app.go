b3716a2d9a1f551d0ffe97d1396d04cc2ad7671d
Levente Blanar <blanar.levente@gmail.com>
2025-07-22 14:01:13 +0200

    3rd round

--- CourseNotes/02_booking_app/booking_app.go
+++ CourseNotes/02_booking_app/booking_app.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	var conferenceName string = "Go Conference"
 	const conferenceTickets int = 50
-	var remainingTickets int = 50
+	var remainingTickets uint = 50
 
 	// %T is a type placeholder
 	fmt.Printf("conferenceTickets is %T, remaningsTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
@@ -15,11 +15,34 @@ func main() {
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var userName string
+	// setting an array - size, type
+	var bookings []string
+
+	var firstName string
+	var lastName string
+	var email string
 	var userTickets int
 	// ask user for their name
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+	
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
 
-	userName = "Tom"
-	userTickets = 2
-	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
-}  
+	fmt.Println("Enter your email address: ")
+	fmt.Scan(&email)
+	
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	remainingTickets = remainingTickets - uint(userTickets)
+	bookings = append(bookings, firstName + " " + lastName)
+
+	fmt.Printf("The whole array: %v\n", bookings)
+	fmt.Printf("The first value: %v\n", bookings[0])
+	fmt.Printf("Array type: %T\n", bookings)
+	fmt.Printf("Array length: %v\n", len(bookings))
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v. \n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaning for %v\n", remainingTickets, conferenceName)
+}
