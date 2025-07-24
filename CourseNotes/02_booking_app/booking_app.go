459e7bb8024557f3a08948067a095423a176cb93
Levente Blanar <blanar.levente@gmail.com>
2025-07-24 09:59:03 +0200

    golang learn session

--- CourseNotes/02_booking_app/booking_app.go
+++ CourseNotes/02_booking_app/booking_app.go
@@ -1,11 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	var conferenceName string = "Go Conference"
 	const conferenceTickets int = 50
 	var remainingTickets uint = 50
+	bookings := []string{}
+
 
 	// %T is a type placeholder
 	fmt.Printf("conferenceTickets is %T, remaningsTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
@@ -16,33 +21,42 @@ func main() {
 	fmt.Println("Get your tickets here to attend")
 
 	// setting an array - size, type
-	var bookings []string
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets int
-	// ask user for their name
-	fmt.Println("Enter your first name: ")
-	fmt.Scan(&firstName)
-	
-	fmt.Println("Enter your last name: ")
-	fmt.Scan(&lastName)
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets int
+		// ask user for their name
+		fmt.Println("Enter your first name: ")
+		fmt.Scan(&firstName)
+		
+		fmt.Println("Enter your last name: ")
+		fmt.Scan(&lastName)
 
-	fmt.Println("Enter your email address: ")
-	fmt.Scan(&email)
-	
-	fmt.Println("Enter number of tickets: ")
-	fmt.Scan(&userTickets)
+		fmt.Println("Enter your email address: ")
+		fmt.Scan(&email)
+		
+		fmt.Println("Enter number of tickets: ")
+		fmt.Scan(&userTickets)
 
-	remainingTickets = remainingTickets - uint(userTickets)
-	bookings = append(bookings, firstName + " " + lastName)
+		remainingTickets = remainingTickets - uint(userTickets)
+		bookings = append(bookings, firstName + " " + lastName)
 
-	fmt.Printf("The whole array: %v\n", bookings)
-	fmt.Printf("The first value: %v\n", bookings[0])
-	fmt.Printf("Array type: %T\n", bookings)
-	fmt.Printf("Array length: %v\n", len(bookings))
+		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v. \n", firstName, lastName, userTickets, email)
+		fmt.Printf("%v tickets remaning for %v\n", remainingTickets, conferenceName)
 
-	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v. \n", firstName, lastName, userTickets, email)
-	fmt.Printf("%v tickets remaning for %v\n", remainingTickets, conferenceName)
+		firstNames := []string{}
+		for _, booking := range bookings { 				// underscore identifies unused variable
+			var names = strings.Fields(booking)
+			firstNames = append(firstNames, names[0])
+		}
+		fmt.Printf("These are all our bookings: %v\n", firstNames)
+
+		if remainingTickets == 0 {
+			// function to end program
+			fmt.Println("Our conference is booked out. Come back next year.")
+			break
+		}
+	}
 }
