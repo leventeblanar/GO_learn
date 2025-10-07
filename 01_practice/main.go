dd73e57de0257f36308949576661573544a9f58e
Levente Blanar <blanar.levente@gmail.com>
2025-10-07 15:51:07 +0200

    pratice sess

--- 01_practice/main.go
+++ 01_practice/main.go
@@ -1,27 +1,58 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-
-	"golang.org/x/exp/constraints"
+	"log"
+	"os"
+	"strings"
 )
 
-// generikus maxValue
-func maxValue[T constraints.Ordered](nums []T) T {
-	max := nums[0]
-	for _, n := range nums[1:] {
-		if n > max {
-			max = n
-		}
-	}
-	return max
+type Product struct {
+	Name string
 }
 
+func (p Product) String() string { // hogy %s-sel is lehessen írni
+	return p.Name
+}
 
 func main() {
-	ints := []int{1, 7, 3, 5}
-	floats := []float64{1.2, 5.4, 3.3}
+	reader := bufio.NewReader(os.Stdin)
+	var productList []Product
 
-	fmt.Println(maxValue(ints))
-	fmt.Println(maxValue(floats))
+	fmt.Println("Enter products to create product list. Type 'done' to finish.")
+
+	for {
+		fmt.Print("> ") // kis prompt
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal("read error:", err)
+		}
+		product := strings.TrimSpace(line)
+
+		if strings.EqualFold(product, "done") {
+			break
+		}
+		if product == "" {
+			if len(productList) == 0 {
+				fmt.Println("The list is empty.")
+			} else {
+				fmt.Println("Enter a product.")
+			}
+			continue
+		}
+
+		productList = append(productList, Product{Name: product})
+	}
+
+	if len(productList) == 0 {
+		fmt.Println("The list is empty. Nothing to show.")
+		return
+	}
+
+	fmt.Println("\nYour shopping list:")
+	for i, p := range productList {
+		fmt.Printf("%d. %s\n", i+1, p)
+	}
+	fmt.Println("Total items:", len(productList))
 }
