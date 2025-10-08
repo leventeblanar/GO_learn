f91109337b1e0708351f130f32419a124c146ac3
Levente Blanar <blanar.levente@gmail.com>
2025-10-08 09:41:26 +0200

    range over iterators

--- 01_practice/main.go
+++ 01_practice/main.go
@@ -1,58 +1,115 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"log"
-	"os"
-	"strings"
+	"time"
 )
 
-type Product struct {
-	Name string
+//	Go-ban a range kulcsoszóval általában kollekciókat járunk be
+
+//	for i, v := range []int{10, 20, 30} {
+//	fmt.Println(i, v)}
+
+//	ez működik szeletekkel, mapekkel, stringekkel, csatornákkal - de eddig nem működött olyan saját típusokkal, amiket te hozol létre (pl. egy saját iterátor)
+
+// 	Miért kell range over iterators?
+// 	korábban csak két lehetőség (Channel -a go routine küldi, a range olvassa || Next() metódus - explicit, kézzel kell ciklizni)
+
+//			for v := range MyGenerator() {
+// 				fmt.Println(v)
+// 			}
+// ez pl egy range function egy függvényen iterál és addig hívogatja a "yield" hívást amíg az vissza nem tér false-al
+
+
+//////////////////////////////////////////////
+/// 	1. Channel alapú iterátor
+//////////////////////////////////////////////
+
+
+// 	CountwithChannel visszaad egy <- chan int-et
+//	a goroutine tölti fel a számokat, a for-range olvassa
+func CountWithChannel(n int) <-chan int {
+	ch := make(chan int)
+
+	go func() {
+		defer close(ch)  				// amikor vége bezárjuka. csatornát
+		for i := 1; i <= n; i++ {
+			ch <- 1						// küldjük a számokat
+			time.Sleep(100 * time.Millisecond)  //csak hogy látni lehessen a sorrendet
+		}
+	}()
+	return ch							// visszaadjuk a csatornát
 }
 
-func (p Product) String() string { // hogy %s-sel is lehessen írni
-	return p.Name
+
+
+
+
+//////////////////////////////////////////////
+/// 	2. NEXT() Metódusos iterátor
+//////////////////////////////////////////////
+
+// Struct tárolja az állapotot
+type Counter struct {
+	current			int
+	limit			int
 }
+
+// konstruktor
+func NewCounter(limit int) *Counter {
+	return &Counter{current:0, limit: limit}
+}
+
+// Next() egyenként adja a számokat
+// Ha elérte a határt, false-szal jelzi a végét
+func (c *Counter) Next() (int, bool) {
+	if c.current >= c.limit {
+		return 0, false
+	}
+	c.current++
+	return c.current, true
+}
+
+
+
+
+
+//////////////////////////////////////////////
+/// 	3. Range-over-func (Go 1.22+)
+//////////////////////////////////////////////
+
+//CountWithRangeFunc visszaad egy olyan függvényt
+// amit a range be tud járni
+func CountWithRangeFunc(n int) func(yield func(int) bool) {
+	return func(yield func(int) bool) {
+		for i := 1; i <= n; i++ {
+			if !yield(i) {				// ha a for-range leállítja
+				return
+			}
+		}
+	}
+}
+
+
+
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-	var productList []Product
 
-	fmt.Println("Enter products to create product list. Type 'done' to finish.")
-
-	for {
-		fmt.Print("> ") // kis prompt
-		line, err := reader.ReadString('\n')
-		if err != nil {
-			log.Fatal("read error:", err)
-		}
-		product := strings.TrimSpace(line)
-
-		if strings.EqualFold(product, "done") {
-			break
-		}
-		if product == "" {
-			if len(productList) == 0 {
-				fmt.Println("The list is empty.")
-			} else {
-				fmt.Println("Enter a product.")
-			}
-			continue
-		}
-
-		productList = append(productList, Product{Name: product})
+	fmt.Println("\n--- Chanel alapú iterátor ---")
+	for v := range CountWithChannel(5) {
+		fmt.Println(v)
 	}
 
-	if len(productList) == 0 {
-		fmt.Println("The list is empty. Nothing to show.")
-		return
+
+	fmt.Println("\n--- Next() metódusos iterátor ---")
+	c := NewCounter(5)
+	for val, ok := c.Next(); ok; val, ok = c.Next() {
+		fmt.Println(val)
 	}
 
-	fmt.Println("\nYour shopping list:")
-	for i, p := range productList {
-		fmt.Printf("%d. %s\n", i+1, p)
+	
+	fmt.Println("\n--- Range-over-func iterator ---")
+	for v:= range CountWithRangeFunc(5) {
+		fmt.Println(v)
 	}
-	fmt.Println("Total items:", len(productList))
 }
